5e7c654d9fc8fb2bf0e50bde1e4da2d9a9f45d7a
aiyc <1432803776@qq.com>
2022-06-09 10:40:23 +0800

    update

--- Coder04/src/MathCalc.java
+++ Coder04/src/MathCalc.java
@@ -2,7 +2,7 @@ public class MathCalc {
     public static void main(String[] args) {
         System.out.println("5+6"); // 双引号是个字符串
         System.out.println(5 + 6);
-        System.out.println(5 + 6);
+        System.out.println(5 - 6);
         System.out.println(5 + 6);
         System.out.println(5 + 6);
     }
