3d8ed10cf457f976666df5db0780a727176f051a
aiyc <1432803776@qq.com>
2022-06-09 10:37:13 +0800

    first commit

---
+++ Coder04/src/MathCalc.java
@@ -0,0 +1,9 @@
+public class MathCalc {
+    public static void main(String[] args) {
+        System.out.println("5+6"); // 双引号是个字符串
+        System.out.println(5 + 6);
+        System.out.println(5 + 6);
+        System.out.println(5 + 6);
+        System.out.println(5 + 6);
+    }
+}
---
+++ QuickSand/src/QuickSand.java
@@ -0,0 +1,5 @@
+public class QuickSand {
+    public static void main(String[] args){
+        System.out.println("QuickSand!");
+    }
+}
