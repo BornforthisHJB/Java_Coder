009577f58f2485ccceeba6764a6690df62347ae0
AndersonHJB <bornforthis@bornforthis.cn>
2022-08-07 19:13:47 +0800

    update

--- Coder14/src/CalcDivFor.java
+++ Coder14/src/CalcDivFor.java
@@ -3,12 +3,14 @@ public class CalcDivFor {
         int divided = 100;
         int divisor = 3;
 
-        for (int i = 0; i < 100; i++) {
+        int found = 0;
+        for (int i = 0; i < 100 && found < 10; i++) {
             if (divided % divisor == 0) {
                 System.out.println(divided + "可以整除" + divisor + "。商为" + divided / divisor);
+                found++;  // 前 ++ 和 后 ++ 影响都不大，前 ++ 与 后 ++，其实是在运行的那行才会体现区别
+//                ++found;
             }
             divided++;
-
         }
 
 
--- Coder14/src/demo.java
+++ Coder14/src/demo.java
@@ -1,7 +1,26 @@
+//public class demo {
+//    public static void main(String[] args) {
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println("i的值是：" + i);
+//        }
+//    }
+//}
+
 public class demo {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("i的值是：" + i);
+        int divided = 100;
+        int divisor = 3;
+
+        int found = 0;
+//        下面这样写可以吗？
+//        不行，逻辑出错，found 需要在可以取整的时候自增，现在这样编写，意味着不管能不能整除都可以自增
+        for (int i = 0; i < 100 && found < 10; i++, found++) {
+            if (divided % divisor == 0) {
+                System.out.println(divided + "可以整除" + divisor + "。商为" + divided / divisor);
+            }
+            divided++;
         }
+
+
     }
 }
