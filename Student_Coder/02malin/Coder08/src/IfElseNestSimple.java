7307177a12dae1608cccc92880dbb8feb52928d4
AndersonHJB <cleland1432803776@icloud.com>
2022-12-12 09:47:44 +0800

    简化

--- Student_Coder/02malin/Coder08/src/IfElseNestSimple.java
+++ Student_Coder/02malin/Coder08/src/IfElseNestSimple.java
@@ -7,4 +7,26 @@
  * @Blog: https://bornforthis.cn
  */
 public class IfElseNestSimple {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+        int c = 99;
+
+        System.out.println("a=" + a + ".b=" + b + ".c=" + c);
+        if (a == b && b == c) {
+            System.out.println("a,b,c等大");
+        } else if (a > b && a > c) {
+            System.out.println("max value is a.");
+        } else if (b > a && b > c) {
+            System.out.println("b is max value");
+        } else if (c > a && c > b) {
+            System.out.println("c is max value.");
+        } else if (a == b && b > c) {
+            System.out.println("a and b is ==");
+        } else if (b == c && b > a) {
+            System.out.println("b and c 等大最大");
+        } else if (a == c && b > c) {
+            System.out.println("a and c is 等大");
+        }
+    }
 }
